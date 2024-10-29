99aa4368d1f73016ce958ac14b7639c186c7b02b
artemgon <artemhoncharuk123@gmail.com>
2024-10-29 17:56:22 +0200

    Project is done

--- main.cpp
+++ main.cpp
@@ -2,11 +2,27 @@
 
 int main()
 {
-	Product newProduct("Guitar", 1400, 10);
+	/*Product newProduct("Guitar", 1400, 10);
 	newProduct.GetInfo();
 	DiscountedProduct newDiscountedProduct("Electric Bass Guitar", 2300, 10, 15);
 	newDiscountedProduct.GetInfo();
 	Product* productPtr = &newDiscountedProduct;
-	productPtr->GetInfo();
+	productPtr->GetInfo();*/
+    shared_ptr<Product> guitar = make_shared<Product>("Guitar", 1400, 10);
+    shared_ptr<DiscountedProduct> electricBass = make_shared<DiscountedProduct>("Electric Bass Guitar", 2300, 10, 15);
+    cout << guitar->GetInfo();
+    cout << electricBass->GetInfo();
+    Product* productPtr = electricBass.get(); 
+    cout << productPtr->GetInfo();
+    ShoppingCart cart;
+    cart.addProduct(guitar);
+    cart.addProduct(electricBass);
+    cout << "Products in the cart:\n";
+    cart.getProducts();
+    User user(1, "Artem");
+    user.addToCart(guitar, cart);
+    user.addToCart(electricBass, cart);
+    user.placeOrder(cart);
+    user.viewOrderHistory();
 	return 0;
 }
